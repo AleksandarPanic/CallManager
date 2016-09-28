7509f48dfdfb031eb6d588f62635385717d1e0ee
AleksandarPanic <aleksandar.panic85@gmail.com>
2016-09-28 15:44:52 +0200

    changes it sample alarm

--- app/src/main/java/net/telenor/callmanager/callmanager/MainActivity.java
+++ app/src/main/java/net/telenor/callmanager/callmanager/MainActivity.java
@@ -1,25 +1,150 @@
 package net.telenor.callmanager.callmanager;
 
-import android.Manifest;
-import android.app.Activity;
+
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.Spinner;
+import android.widget.TextView;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+import android.widget.ArrayAdapter;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
-public class MainActivity extends Activity {
+
+
+public class MainActivity extends AppCompatActivity implements Spinner.OnItemSelectedListener{
+
+    //Declaring an Spinner
+    private Spinner spinner;
+
+    //An ArrayList for Spinner Items
+    private ArrayList<String> brojevi;
+
+    //JSON Array
+    private JSONArray result;
+
+    //TextViews to display details
+    private TextView textViewPoziv;
+    private TextView textViewSms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initializing the ArrayList
+        brojevi = new ArrayList<String>();
+
+        //Initializing Spinner
+        spinner = (Spinner) findViewById(R.id.spinner);
+
+        //Adding an Item Selected Listener to our Spinner
+        //As we have implemented the class Spinner.OnItemSelectedListener to this class iteself we are passing this to setOnItemSelectedListener
+        spinner.setOnItemSelectedListener(this);
+
+        //Initializing TextViews
+        textViewPoziv = (TextView) findViewById(R.id.textViewPoziv);
+        textViewSms = (TextView) findViewById(R.id.textViewSms);
+
+        //This method will fetch the data from the URL
+        getData();
+
     }
+
+    private void getData(){
+        //Creating a string request
+        StringRequest stringRequest = new StringRequest(Config.DATA_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        JSONObject j = null;
+                        try {
+                            //Parsing the fetched Json String to JSON Object
+                            j = new JSONObject(response);
+
+                            //Storing the Array of JSON String to our JSON Array
+                            result = j.getJSONArray(Config.JSON_ARRAY);
+
+                            //Calling method getStudents to get the students from the JSON Array
+                            getStudents(result);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+
+        //Creating a request queue
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        //Adding request to the queue
+        requestQueue.add(stringRequest);
+    }
+
+    private void getStudents(JSONArray j){
+        //Traversing through all the items in the json array
+        for(int i=0;i<j.length();i++){
+            try {
+                //Getting json object
+                JSONObject json = j.getJSONObject(i);
+
+                //Adding the name of the student to array list
+                brojevi.add(json.getString(Config.TAG_PHONE));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //Setting adapter to show the items in the spinner
+        spinner.setAdapter(new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, brojevi));
+    }
+
+    //Method to get student name of a particular position
+    private String getName(int position){
+        String name="";
+        try {
+            //Getting object of given index
+            JSONObject json = result.getJSONObject(position);
+
+            //Fetching name from that object
+            name = json.getString(Config.TAG_POZIV);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        //Returning the name
+        return name;
+    }
+
+    //Doing the same with this method as we did with getName()
+    private String getCourse(int position){
+        String course="";
+        try {
+            JSONObject json = result.getJSONObject(position);
+            course = json.getString(Config.TAG_SMS);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return course;
+    }
+
+
 
     public void startAlert(View view) {
         EditText text = (EditText) findViewById(R.id.time);
@@ -28,5 +153,17 @@ public class MainActivity extends Activity {
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this.getApplicationContext(), 234324243, intent, 0);
         AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
         alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + (i * 1000), pendingIntent);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        textViewPoziv.setText(getName(position));
+        textViewSms.setText(getCourse(position));
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        textViewPoziv.setText("");
+        textViewSms.setText("");
     }
 }
--- app/src/main/java/net/telenor/callmanager/callmanager/SampleAlarmReceiver.java
+++ app/src/main/java/net/telenor/callmanager/callmanager/SampleAlarmReceiver.java
@@ -5,8 +5,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.WakefulBroadcastReceiver;
+
+
+
 
 /**
  * Created by apanic on 9/27/2016.
@@ -16,10 +20,12 @@ public class SampleAlarmReceiver extends WakefulBroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+
+
         PackageManager pm = context.getPackageManager();
         int hasPerm = pm.checkPermission(Manifest.permission.CALL_PHONE,context.getPackageName());
         if (ContextCompat.checkSelfPermission(context, Manifest.permission.CALL_PHONE)== PackageManager.PERMISSION_GRANTED){
-            Intent myIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "+38163670832"));;
+            Intent myIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "+38163670695"));;
             myIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(myIntent);
         }
